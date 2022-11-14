a085198d41c7a0384fabef68c391b0d178f6409a
PedroH183 <pedro_freires@outlook.com>
2022-11-14 18:09:01 -0300

    drawer implmentador por completo, falta estilizar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Tabela, AddButton} from './components/components';
-import { Drawer } from './components/drawer';
+import { Drawer, DrawerContainer } from './components/drawer';
 import './App.css';
 
 export const App = () => {
   /* o state do drawer deve ficara aqui para que possa ser desvinculado do componente */
+  const [ open, setOpen ] = useState(false);
 
-  
   return (
     <div className='pagina'>
 
+      <DrawerContainer status={open} handleChangeStatus={setOpen}/>
+      
       <div className='NavBar'>
-        <Drawer />
+        <Drawer status={open} handleChangeStatus={setOpen}/>
         <h2>Tabela</h2>
       </div>
 
--- src/components/drawer.jsx
+++ src/components/drawer.jsx
@@ -3,30 +3,45 @@ import drawerFig  from '../assets/icons/drawer/drawer4.png';
 import arrowBack  from '../assets/icons/drawer/arrowBack.png';
 
 
-const DrawerContainer = (props) => {
-    let changeStatus = props.changeStatus;
+export const DrawerContainer = (props) => {
     let status = props.status;
 
+    const handleClose = () => {
+        props.handleChangeStatus(false);
+    }
 
     return(
         <>
-            
+            {
+                status && 
+                <div className='DrawerContainer'>
+
+                    <div className='DrawerExit'>
+                        <input 
+                        type='image'
+                        alt='DrawerFigExit'
+                        src={arrowBack}
+                        className='DrawerExitFig'
+                        onClick={ () => handleClose()}/>
+                    </div>
+
+                    <div className='DrawerHeader'>
+                        <label>Container do Drawer</label>
+                    </div>
+
+                </div>
+            }
         </>
     );
 }
 
 
-export const Drawer = () => {
-    /* terá que elevar o state para fora do componente   */
-    const [ open, setOpen ] = useState(false);
-
-    const handleCloseDrawer = () =>{
-        setOpen(false);
-    }
-
-    const handleOpenDrawer = () =>{
-        setOpen(true);
-    }
+export const Drawer = (props) => {
+    const open = props.status;
+    
+    const handleOpen = () => {
+        props.handleChangeStatus(true);
+    } 
 
     return (
         <>
@@ -38,28 +53,9 @@ export const Drawer = () => {
                         alt='imagemDrawer'
                         src={drawerFig}
                         className='DrawerEnterFig'
-                        onClick={ () => handleOpenDrawer()} />
+                        onClick={ () => handleOpen()} />
                 </div>
             }  
-            {
-                open && 
-                <div className='DrawerContainer'>
-
-                    <div className='DrawerExit'>
-                        <input 
-                        type='image'
-                        alt='DrawerFigExit'
-                        src={arrowBack}
-                        className='DrawerExitFig'
-                        onClick={ () => handleCloseDrawer()}/>
-                    </div>
-
-                    <div className='DrawerHeader'>
-                        <label>Container do Drawer</label>
-                    </div>
-
-                </div>
-            }
         </>
         
   );
